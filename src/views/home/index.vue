30b0dbdb5cf2588be7f334f6420b7b40b7eda797
cxh <564668501@qq.com>
2020-02-13 13:08:20 +0800

    文章列表独立

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,22 +4,8 @@
     <van-tabs v-model="activeChannelIndex">
       <!-- title="xxx"：标签名称 -->
       <van-tab title="推荐">
-        <div class="scroll-wrapper">
-          <!-- van-list --- 上拉动作（瀑布流效果） -->
-          <!--
-          v-model="loading"：加载动画效果（加载中···）
-          :finished="finished"：是否停止加载，false继续，true停止
-          finished-text="没有更多了"：停止加载的文字提示
-          @load="onLoad"：加载数据的回调事件，页面初次加载/滚轮滚动到系统设置的页面底部 都会自动触发
-          -->
-          <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <!--
-            van-cell - 单元格组件，内容独占一行进行显示，使用别的div也可以
-                      :title="item":单元格标题
-            -->
-            <van-cell v-for="item in list" :key="item" :title="item" />
-          </van-list>
-        </div>
+        <!-- 使用 com-article 组件 -->
+        <com-article></com-article>
       </van-tab>
       <van-tab title="数据库">数据库1</van-tab>
       <van-tab title="后端">后端1</van-tab>
@@ -28,35 +14,18 @@
 </template>
 
 <script>
+// 导入com-article组件
+import ComArticle from './components/com-article.vue'
 export default {
   name: 'home-index',
+  components: {
+    // 注册 com-article
+    ComArticle
+  },
   data () {
     return {
-      // --瀑布流相关成员
-      list: [], // 加收加载好的数据
-      loading: false, // ‘加载中···’动画是否显示（加载的时候设置为true，加载完毕之后设置为false），每次onLoad执行就设置成true，执行完毕之后为false
-      finished: false, // 加载是否停止的标志，false可以继续加载，true瀑布流停止加载，如果后端没有数据可以提供了，就设置该项目为true即可
       // --通过下标设置激活频道项目，下标从0开始
       activeChannelIndex: 0
-    }
-  },
-  methods: {
-    onLoad () {
-      // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-
-        // 加载状态结束
-        this.loading = false // 加载中···动画清除
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true // 瀑布流停止加载
-        }
-      }, 1000)
     }
   }
 }
@@ -76,12 +45,6 @@ export default {
   }
   /deep/ .van-tab__pane {
     height: 100%;
-    // 给上拉列表设置样式
-    .scroll-wrapper {
-      height: 100%;
-      // 内容过多，有垂直滚动条
-      overflow-y: auto;
-    }
   }
   // 给频道下边沿横向设置样式
   /deep/ .van-tabs__line {
