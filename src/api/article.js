c897cace7382d2a955c811843f9296a0d0001a15
cxh <564668501@qq.com>
2020-02-15 11:06:02 +0800

    文章和频道-文章举报-api封装

--- src/api/article.js
+++ src/api/article.js
@@ -3,6 +3,26 @@ import request from '@/utils/request.js'
 
 // 创建各个api方法
 /**
+ * 对文章做举报处理
+ * @param {文章id} articleID
+ * @param {举报类型} type
+ * 举报类型： 0-其他问题，1-标题夸张，2-低俗色情，3-错别字多，4-旧闻重复，5-广告软文，6-内容不实，
+ * 7-涉嫌违法犯罪，8-侵权'
+ * {articleID, type, remark=''} 是对象解构赋值用法，这样可以清楚知道当前接口需要哪些参数，
+ * 不用再查看接口文档了，非常方便
+ */
+export function apiArticleReport ({ articleID, type, remark = '' }) {
+  return request({
+    url: '/app/v1_0/article/reports',
+    method: 'post',
+    data: {
+      target: articleID,
+      type,
+      remark // 其他问题 的附加说明
+    }
+  })
+}
+/**
  * 对不感兴趣(不喜欢)的文章做处理
  * @param {文章id} articleID
  */
