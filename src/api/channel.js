d26dcea7025ca870793dd4e7cb17fb8facbbf3ba
cxh <564668501@qq.com>
2020-02-17 10:59:05 +0800

    频道本地持久化

--- src/api/channel.js
+++ src/api/channel.js
@@ -1,24 +1,53 @@
 // 导入axios模块
 import request from '@/utils/request.js'
-
+// 导入store
+import store from '@/store' // 以便知道用户是否登录系统
+// 本地持久化存储频道设置的key（游客  和  登录用户 分别设置）
+const CHANNEL_KEY_TRAVEL = 'hm-channel-travel' // 游客key
+const CHANNEL_KEY_VIP = 'hm-channel-vip' // 登录用户key
 // 创建各个api方法
+
+// 频道列表
+export function apiChannelList () {
+  // 通过Promise封装，通过resolve返回输出具体信息，
+  // 因为所有api接口的返回结果都是Promise对象
+  return new Promise(async (resolve) => {
+    // 判断用户是否登录，并执行不同的key
+    const key = store.state.user.token ? CHANNEL_KEY_VIP : CHANNEL_KEY_TRAVEL
+    // 获得 本地频道数据
+    const localChannels = localStorage.getItem(key)
+    if (localChannels) {
+      // 如果有，输出给外部
+      resolve({ channels: JSON.parse(localChannels) })
+    } else {
+      // 如果没有，发送axios请求
+      const res = await request({
+        url: '/app/v1_0/user/channels',
+        method: 'get'
+      })
+      // 本地存储频道数据
+      localStorage.setItem(key, JSON.stringify(res.channels))
+      // 输出频道数据给外部
+      resolve(res)
+    }
+  })
+}
+// export function apiChannelList () {
+//   // 请求axios，request就是axios的复制品，操作结构完全一致
+//   // return返回执行结果，是promise对象
+//   return request({
+//     // url:'差异化url地址'
+//     url: '/app/v1_0/user/channels',
+//     method: 'get'
+//     // params:get请求成员标志
+//     // data:非get请求成员标志
+//   })
+// }
+
 // 全部频道列表
 export function apiChannelAll () {
   return request({
     url: '/app/v1_0/channels',
     method: 'get'
-  })
-}
-
-// 频道列表
-export function apiChannelList () {
-  // 请求axios，request就是axios的复制品，操作结构完全一致
-  // return返回执行结果，是promise对象
-  return request({
-    // url:'差异化url地址'
-    url: '/app/v1_0/user/channels',
-    method: 'get'
-    // params:get请求成员标志
-    // data:非get请求成员标志
   })
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     redirect: '/home', // 重定向
-    component: () => import('@/views/layout/index.vue'),  // 一级路由 布局组件
+    component: () => import('@/views/layout/index.vue'), // 一级路由 布局组件
     children: [
       { path: '/home', name: 'home', component: () => import('@/views/home/index.vue') }, // 二级路由 首页组件
       { path: '/question', component: () => import('@/views/question/index.vue') }, // 二级路由 问答组件
       { path: '/video', component: () => import('@/views/video/index.vue') }, // 二级路由 视频组件
-      { path: '/user', component: () => import('@/views/user/index.vue') }, // 二级路由 个人中心组件
+      { path: '/user', component: () => import('@/views/user/index.vue') } // 二级路由 个人中心组件
     ]
   },
   { path: '/user/profile', component: () => import('@/views/user/profile.vue') }, // 一级路由 编辑资料组件
@@ -21,7 +20,7 @@ const routes = [
   { path: '/login', name: 'login', component: () => import('@/views/user/login.vue') }, // 一级路由 登录组件
   { path: '/search', component: () => import('@/views/search/index.vue') }, // 一级路由 搜索中心组件
   { path: '/search/result', component: () => import('@/views/search/result.vue') }, // 一级路由 搜索结果组件
-  { path: '/article', component: () => import('@/views/article/index.vue') }, // 一级路由 文章详情组件
+  { path: '/article', component: () => import('@/views/article/index.vue') } // 一级路由 文章详情组件
 ]
 
 const router = new VueRouter({
