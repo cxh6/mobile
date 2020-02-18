c6a214a34b6730932dd6e1f6fe6e58676e6778fe
cxh <564668501@qq.com>
2020-02-18 18:05:21 +0800

    文章详情-内容渲染

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -3,28 +3,24 @@
     <van-nav-bar fixed left-arrow @click-left="$router.back()" title="文章详情"></van-nav-bar>
     <!-- 文章详情区域 -->
     <div class="detail">
-      <h3 class="title">文章标题</h3>
+      <h3 class="title">{{article.title}}</h3>
       <div class="author">
         <!-- 头像 -->
         <!--
          fit='fill'：图片填充模式 默认值fill
         -->
-        <van-image
-          round
-          width="1rem"
-          height="1rem"
-          fit="fill"
-          src="https://img.yzcdn.cn/vant/cat.jpeg"
-        />
+        <van-image round width="1rem" height="1rem" fit="fill" :src="article.aut_photo" />
         <div class="text">
-          <p class="name">作者名字</p>
-          <p class="time">时间</p>
+          <p class="name">{{article.aut_name}}</p>
+          <!-- 使用过滤器 formatTime -->
+          <p class="time">{{article.pubdate | formatTime}}</p>
         </div>
-        <van-button round size="small" type="info">+ 关注</van-button>
+        <van-button round size="small" :type="article.is_followed?'default':'info'">{{article.is_followed?'取消关注':'+ 关注'}}</van-button>
       </div>
       <!-- 文章内容 -->
       <div class="content">
-        <p>文章内容内容...</p>
+        <!-- 获取文章结果的内容中带p标签，这里使用v-html -->
+        <p v-html="article.content"></p>
       </div>
       <!-- 点赞/不喜欢 -->
       <div class="zan">
@@ -32,12 +28,12 @@
         <van-button
           round
           size="small"
-          class="active"
+          :class="{active:article.attitude===1}"
           plain
           icon="like-o"
           style="margin-right:8px;"
         >点赞</van-button>
-        <van-button round size="small" plain icon="delete">不喜欢</van-button>
+        <van-button round size="small" plain icon="delete" :class="{active:article.attitude===0}">不喜欢</van-button>
       </div>
     </div>
   </div>
@@ -50,6 +46,7 @@ export default {
   name: 'article',
   data () {
     return {
+      // is_followed: false, // 是否关注了作者
       article: {} // 文章详情
     }
   },
