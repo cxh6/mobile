7a34e4672a4f330bc6e2e8d4796e462193cae559
cxh <564668501@qq.com>
2020-02-18 13:10:39 +0800

    联想历史记录管理-页面布局

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -3,12 +3,40 @@
     <van-nav-bar title="搜索中心" left-arrow @click-left="$router.back()" />
     <!-- @search="onSearch()"：按下回车执行的事件 -->
     <van-search v-model.trim="searchText" placeholder="请输入搜索关键词" @search="onSearch(searchText)" />
-    <van-cell-group>
+    <!-- 联想建议 -->
+    <van-cell-group v-if="suggestionList.length>0">
       <!-- <van-cell v-for="(item,k) in suggestionList" :key="k" :title="item" icon="search" /> -->
       <!-- @click='onSearch()'  点击，执行onSearch事件 -->
-      <van-cell v-for="(item,k) in suggestionList" :key="k" icon="search" @click='onSearch(item)'>
+      <van-cell v-for="(item,k) in suggestionList" :key="k" icon="search" @click="onSearch(item)">
         <!-- slot="title"使用命名插槽 覆盖 title内容 -->
         <span slot="title" v-html="highlightCell(item,searchText)"></span>
+      </van-cell>
+    </van-cell-group>
+    <!-- 联想历史记录管理 -->
+    <van-cell-group v-else>
+      <van-cell title="历史记录">
+        <!--
+          name="delete" 垃圾桶图标
+          style="line-height:inherit" 高度和父级一样高
+          v-show="!isDeleteData" 控制垃圾桶图标是否显示
+          slot="right-icon" 命名插槽 给 cell单元格的右边显示内容(垃圾桶图标)
+        -->
+        <van-icon
+          @click="isDeleteData=true"
+          v-show="!isDeleteData"
+          name="delete"
+          slot="right-icon"
+          style="line-height:inherit"
+        ></van-icon>
+        <div v-show="isDeleteData">
+          <span style="margin-right:10px;">全部删除</span>
+          <span @click="isDeleteData=false">完成</span>
+        </div>
+      </van-cell>
+      <!-- 历史联想展示 -->
+      <van-cell title="123">
+        <!-- 叉号删除按钮 -->
+        <van-icon v-show="isDeleteData" slot="right-icon" name="close" style="line-height:inherit"></van-icon>
       </van-cell>
     </van-cell-group>
   </div>
@@ -21,6 +49,7 @@ export default {
   name: 'search-index',
   data () {
     return {
+      isDeleteData: false, // 历史记录开关
       suggestionList: [], // 联想到的数据
       searchText: '' // 搜索关键词
     }
